1ae926426c4307bcf8ee5c641f44a6b14d5afa74
Khaled B. Abu Alqomboz <eng.khaledb@gmail.com>
2016-05-06 11:20:31 +0300

    Fix rest response for wrong method requested

--- Base/RestController.php
+++ Base/RestController.php
@@ -7,14 +7,14 @@ class RestController extends Controller
     public function __construct()
     {
         parent::__construct();
-        $methods = array('POST','PUT','GET','DELETE');
+        $methods = array('POST', 'PUT', 'GET', 'DELETE');
         $request_method = $_SERVER['REQUEST_METHOD'];
 
-        if(in_array($request_method,$methods)){
+        if (in_array($request_method, $methods)) {
             $request_method = strtolower($request_method);
             $this->$request_method();
-        }else{
-            Response::json(array(),500);
+        } else {
+            Response::json(array(), 500);
         }
     }
 }
